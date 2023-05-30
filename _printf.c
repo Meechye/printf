31f74f797bebffe723afa7d1dc14eec9738bae98
Mitchelle <mitchelleonyeanusi@gmail.com>
2023-05-30 03:38:21 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,34 +1,66 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
  * _printf - produces output according to a format.
- * @format: A character string
+ *
+ * @format: is a character string
+ *
+ * Description: Receives the main string and all the necessary parameters to
+ * print a formated string
+ *
  * Return: the number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	int count = 0;
-
 	va_list args;
 
 	va_start(args, format);
+	int count = 0; /*counts the number of characters printed*/
 
-	while (*format != '\0')
+	while (*format != '\0') /*as long as the characters are not null byte*/
 	{
-		if (*format == '%')
+		if (*format == '%')  /*note when the program encounters a "%"*/
 		{
-			format++;
+			format++; /*Move past '%'*/
+
+
+			if (*format == 'c')
+			{
+				/*print a character*/
+				int c = va_arg(args, int);
+
+				_putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				/*print a string*/
+				char *s = va_arg(args, char *);
+
+				while (*s != '\0')
+				{
+					_putchar(*s);
+					s++;
+					count++;
+				}
+			}
+			else if (*format == '%')
+			{
+				/*Print '%'*/
+				_putchar('%');
+				count++;				}
+			}
 		}
 		else
 		{
+			/*Print any other character*/
 			_putchar(*format);
 			count++;
-			format++;
 		}
-	}
+		format++;
+
 	va_end(args);
-	return(count);
+	return (count);
+	}
 }
