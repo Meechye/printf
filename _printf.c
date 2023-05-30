024e47868c1311a6a92d19103962adbc3980cb05
Layoamona <amonamarysmith@gmail.com>
2023-05-30 07:40:17 -0700

    updated file

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include "main.h"
 /**
  * _printf - produces output according to a format.
  *
@@ -14,43 +14,46 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
+	
+	int count; /*counts the number of characters printed*/
 
 	va_start(args, format);
-	int count = 0; /*counts the number of characters printed*/
+	
+	count = 0;
+	
 
 	while (*format != '\0') /*as long as the characters are not null byte*/
 	{
 		if (*format == '%')  /*note when the program encounters a "%"*/
 		{
 			format++; /*Move past '%'*/
+		}
 
+		else if (*format == 'c')
+		{
+			/*print a character*/
+			int c = va_arg(args, int);
 
-			if (*format == 'c')
+			_putchar(c);
+			count++;
+		}
+		else if (*format == 's')
+		{
+			/*print a string*/
+			char *s = va_arg(args, char *);
+
+			while (*s != '\0')
 			{
-				/*print a character*/
-				int c = va_arg(args, int);
-
-				_putchar(c);
+				_putchar(*s);
+				s++;
 				count++;
 			}
-			else if (*format == 's')
-			{
-				/*print a string*/
-				char *s = va_arg(args, char *);
-
-				while (*s != '\0')
-				{
-					_putchar(*s);
-					s++;
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				/*Print '%'*/
-				_putchar('%');
-				count++;				}
-			}
+		}
+		else if (*format == '%')
+		{
+			/*Print '%'*/
+			_putchar('%');
+			count++;				
 		}
 		else
 		{
@@ -59,8 +62,8 @@ int _printf(const char *format, ...)
 			count++;
 		}
 		format++;
+	}
 
 	va_end(args);
 	return (count);
-	}
 }
