b187c2f924b4872b5946f77688b11603a27c739d
Mitchelle <mitchelleonyeanusi@gmail.com>
2023-05-30 14:15:34 -0700

    printf.c

--- main.h
+++ main.h
@@ -5,24 +5,12 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct format - format for the struct
- * @specifiers: struct format
- * @f: The function associated
- */
-
-typedef struct specifiers
-{
-	char specifiers;
-	int (*f)(va_list);
-}specifiers_t;
-
 /* prototypes */
 int _printf(const char *format, ...);
 int _putchar(char c);
 
 /* Conversion specifiers */
-int print_string (va_list args);
+int print_string(va_args(args, char *));
 int print_rev_strings(va_list args);
 int print_rot13(va_list args);
 int print_hexadecimal(va_list args);
@@ -32,5 +20,5 @@ int print_char(va_list args);
 int print_binary(va_list args);
 int print_octal(va_list args);
 int print_width(const char *format, int *i, va_list);
-
+int print-decimal(const char *format, ...);
 #endif /* MAIN_H */
